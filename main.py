e85452e115d21776b967ab3214c3fa4060ebc062
v1ce-MB <Mburuvict@Gmail.com>
2024-10-26 17:41:18 +0300

    file handle

---
+++ main.py
@@ -0,0 +1,11 @@
+file = open("john", "w")
+file.write("John is a student in the 11th grade. John is a tall boy. his favourite color is black and likes maths.John is an extrovert and is friendly")
+file.close()
+file = open ("Alexary", "w")
+file.write(" curious by nature, Alex loves to ask questions and dive deep into his studies. His favorite color is blue, which he feels represents the ocean and the sky, bringing him a sense of calm. Outside of academics, he enjoys spending time outdoors, whether hiking or hanging out with friends. Alex is excited to see what this year has in store for him!")
+file.close()
+
+
+file = open("Dennis", "w")
+file.write("Introducing Dennis, a driven 11th grader with a strong passion for physics Dennis is known for his inquisitive nature and problem-solving skills. His favorite color is orange, which he feels represents creativity and enthusiasm. Outside of academics, he enjoys playing soccer with his friends and experimenting with new science projects. Dennis is excited to take on the challenges of this year and deepen his understanding of the world around him!")
+file.close()
